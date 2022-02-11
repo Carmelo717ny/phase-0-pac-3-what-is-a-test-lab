3468eee06f4ad8f5c001839cfe9c2a32f9d727e1
Carmelo Martinez-Vazquez <Carmelo.M.Vazquez@hotmail.com>
2022-02-11 16:09:31 -0500

    commit tree 8addb6689e0f28649a28eaf170a8c7968b6ffa27

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+const name = "Susan";
+const height = "40";
+const message = `${name} is ${height} to be less than (40)`;
 
 module.exports = {name, height, message};
+console.log(message);
